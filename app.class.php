4b4c4465e82f95b32feb31a7a56316e412dc44c4
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-10-09 13:57:22 -0300

    conexion bd

--- app.class.php
+++ app.class.php
@@ -1,12 +1,26 @@
 <?php
 
-class App
-{
-    public function __construct()
-    {
+require_once("db.class.php");
 
+class App {
+
+    private $db;
+
+    public function __construct(){
         $uri = $_SERVER["REQUEST_URI"];
         $uriParts = explode("/", $uri);
         array_shift($uriParts);
+
+        $this->connectDB();
+    }
+
+    public function connectDB(){
+        try {
+            $this->db = DB::getInstance();
+        } catch (Exception $e) {
+            echo "Error al conectar a la base de datos: " . $e->getMessage();
+        }
     }
 }
+
+
