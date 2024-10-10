c2bcf00cce869a19fec761373079dec6e697e466
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-10-10 10:17:08 -0300

    metodo agregar

--- models/products.php
+++ models/products.php
@@ -5,16 +5,44 @@ class Products {
 
     public function __construct($db) {
         $this->db = $db;
-
     }
 
     public function index() {
         $sql = "SELECT * FROM products";
-        $products = $this->db->query($sql);
+        $result = $this->db->query($sql);
 
-        return json_encode($products);
+        $products = [];
+        while ($row = $result->fetch_object()) {
+            $products[] = $row;
+        }
+
+        return new Template("/views/products/index.html", [
+            "products" => $products
+        ]);
+    }
+
+    public function show($id) {
+        $id = $this->db->escape($id);
+        $product = $this->db->queryOne("SELECT * FROM products WHERE product_id = " . $id);
+
+        return new Template("/views/products/show.html", [
+            "product" => $product
+        ]);
+    }
+
+    public function addProduct($name, $price) {
+        $name = $this->db->escape($name);
+        $price = $this->db->escape($price);
+        $sql = "INSERT INTO products (name, price) VALUES ('$name', '$price')";
+        $this->db->query($sql);
     }
 }
+
+
+
+
+
+
 
 
 
